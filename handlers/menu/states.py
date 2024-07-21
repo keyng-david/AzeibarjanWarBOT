1640b3cc65095fa94d9c11537d084511ba7bc515
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-07-21 13:25:49 -0700

    Update states.py

--- handlers/menu/states.py
+++ handlers/menu/states.py
@@ -1,15 +1,15 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 
-from AzeibarjanWarBOT.database import DB
-from AzeibarjanWarBOT.filters.filter import IsPrivate
-from AzeibarjanWarBOT.keyboards import default, inline
-from AzeibarjanWarBOT.loader import dp, bot
-from AzeibarjanWarBOT.src import dicts
-from AzeibarjanWarBOT.state.states import DeleteHero, ChangeStatistic
-from AzeibarjanWarBOT.utils import strings
-from AzeibarjanWarBOT.utils.class_getter import get_user_info
-from AzeibarjanWarBOT.utils.functions import ret_city, get_name_availability
+from database import DB
+from filters.filter import IsPrivate
+from keyboards import default, inline
+from loader import dp, bot
+from src import dicts
+from state.states import DeleteHero, ChangeStatistic
+from utils import strings
+from utils.class_getter import get_user_info
+from utils.functions import ret_city, get_name_availability
 
 
 @dp.message_handler(IsPrivate(), state=DeleteHero.delete_hero)

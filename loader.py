f96c2d38cadf544b26d139d87a5e0a6d9f951829
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-07-21 15:45:08 -0700

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.fsm.storage.memory import MemoryStorage
 import config
 
 bot = Bot(token=config.TOKEN, parse_mode=types.ParseMode.HTML)

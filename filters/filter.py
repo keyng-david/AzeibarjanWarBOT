d9845e9d8e004bf3c2cab6afc10cfea798543a66
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-07-23 13:11:06 -0700

    Update filter.py

--- filters/filter.py
+++ filters/filter.py
@@ -9,57 +9,54 @@ from utils.class_getter import get_user_info
 
 
 class IsPrivate(BaseFilter):
-    async def check(self, m: types.Message):
-        return m.chat.type == types.ChatType.PRIVATE
+    async def __call__(self, message: types.Message) -> bool:
+        return message.chat.type == types.ChatType.PRIVATE
 
 class IsNotPrivate(BaseFilter):
-    async def check(self, m: types.Message):
-        return m.chat.type != types.ChatType.PRIVATE
+    async def __call__(self, message: types.Message) -> bool:
+        return message.chat.type != types.ChatType.PRIVATE
 
 class IsAdmin(BaseFilter):
-    async def check(self, m: types.Message):
-        return m.from_user.id in config.ADMIN_ID
+    async def __call__(self, message: types.Message) -> bool:
+        return message.from_user.id in config.ADMIN_ID
 
 class IsClanHead(BaseFilter):
-    async def check(self, message: types.Message):
+    async def __call__(self, message: types.Message) -> bool:
         user_clan_info = await DB.get_clan_user(message.from_user.id)
         if user_clan_info is not None:
             if user_clan_info[2] == 2:
                 return True
         else:
             await bot.send_message(message.from_user.id, strings.not_have_acces_head)
-
+        return False
 
 class IsClanHeadOrAdmin(BaseFilter):
-    async def check(self, message: types.Message):
+    async def __call__(self, message: types.Message) -> bool:
         user_clan_info = await DB.get_clan_user(message.from_user.id)
         if user_clan_info is not None:
             if user_clan_info[2] == 2 or user_clan_info[2] == 1:
                 return True
         else:
             await bot.send_message(message.from_user.id, strings.not_have_acces)
-
+        return False
 
 class UserInClan(BaseFilter):
-    async def check(self, message: types.Message):
+    async def __call__(self, message: types.Message) -> bool:
         user_clan_info = await DB.get_clan_user(message.from_user.id)
         if user_clan_info is None:
             await bot.send_message(message.from_user.id, strings.not_in_clan)
             return False
         return True
 
-
 class UserNotInClan(BaseFilter):
-    async def check(self, message: types.Message):
+    async def __call__(self, message: types.Message) -> bool:
         user_clan_info = await DB.get_clan_user(message.from_user.id)
         if user_clan_info is not None:
             await bot.send_message(message.from_user.id, strings.you_already_in_clan)
             return False
         return True
 
-
-
 class UserInCity(BaseFilter):
-    async def check(self, message: types.Message):
+    async def __call__(self, message: types.Message) -> bool:
         user_info = await get_user_info(message.from_user.id)
         return user_info.city in strings.city_list[:-1]

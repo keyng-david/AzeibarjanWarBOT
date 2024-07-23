93620d965b6442c6998e80310356b86e8df30b0d
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-07-23 13:52:20 -0700

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,12 +1,12 @@
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-import config
 from aiogram.enums import ParseMode
+import config
 
 print("Token:", config.TOKEN)  # Ensure the token is correctly loaded
 
 if not config.TOKEN:
     raise ValueError("Bot token is not provided or invalid")
 
-bot = Bot(token=config.TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(token=config.TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher(storage=MemoryStorage())
